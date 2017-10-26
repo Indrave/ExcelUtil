bf74848a13d5c10adfae4a4443a5c40d87fc84a8
Indrave <806344324@qq.com>
2017-10-26 17:57:52 +0800

    初始化一些Excel数据

--- src/com/indrave/util/ExcelXlsReader.java
+++ src/com/indrave/util/ExcelXlsReader.java
@@ -1,4 +1,22 @@
 package com.indrave.util;
 
-public class ExcelXlsReader {
+import org.apache.poi.hssf.eventusermodel.HSSFListener;
+import org.apache.poi.hssf.record.Record;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+public class ExcelXlsReader implements HSSFListener {
+
+    private int minColumns = -1;
+
+    private POIFSFileSystem fs;
+
+    private int lastRowNumber;
+
+    private int lastColumnNumber;
+
+
+    @Override
+    public void processRecord(Record record) {
+
+    }
 }
