5b8ac994626f958621f995833fabc70421fd89e3
Indrave <806344324@qq.com>
2017-10-26 23:02:17 +0800

    初始化一些Excel数据

--- src/com/indrave/util/ExcelXlsReader.java
+++ src/com/indrave/util/ExcelXlsReader.java
@@ -1,8 +1,17 @@
 package com.indrave.util;
 
+import com.indrave.service.IExcelRowReader;
+import org.apache.poi.hssf.eventusermodel.EventWorkbookBuilder.SheetRecordCollectingListener;
+import org.apache.poi.hssf.eventusermodel.FormatTrackingHSSFListener;
 import org.apache.poi.hssf.eventusermodel.HSSFListener;
+import org.apache.poi.hssf.record.BoundSheetRecord;
 import org.apache.poi.hssf.record.Record;
+import org.apache.poi.hssf.record.SSTRecord;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExcelXlsReader implements HSSFListener {
 
@@ -13,6 +22,40 @@ public class ExcelXlsReader implements HSSFListener {
     private int lastRowNumber;
 
     private int lastColumnNumber;
+
+    private boolean outputFormulaValue = true;
+
+    private SheetRecordCollectingListener workbookBuildingListen;
+
+    private HSSFWorkbook stubWorkbook;
+
+    private SSTRecord sstRecord;
+
+    private FormatTrackingHSSFListener formatTrackingHSSFListener;
+
+    //表索引
+    private int sheetIndex = -1;
+
+    private BoundSheetRecord[] boundSheetRecord;
+
+    private List boundSheetRecords = new ArrayList();
+
+    private int nextRow;
+
+    private int nextColumn;
+
+    private boolean outputNextStringRecord;
+
+    // 当前行
+    private int curRow = 0;
+
+    // 存储行记录的容器
+    private List<String> rowlist = new ArrayList<String>();
+
+    @SuppressWarnings("unused")
+    private String sheetName;
+
+    private IExcelRowReader rowReader;
 
 
     @Override
