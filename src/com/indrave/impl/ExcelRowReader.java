b795a9d3998a4ea88e447f0927c12deb7716ccb6
Indrave <806344324@qq.com>
2017-11-26 21:36:03 +0800

    create:Excel2003版本及以下读取

--- src/com/indrave/impl/ExcelRowReader.java
+++ src/com/indrave/impl/ExcelRowReader.java
@@ -5,6 +5,7 @@ import com.indrave.service.IExcelRowReader;
 import java.util.List;
 
 public class ExcelRowReader implements IExcelRowReader {
+
     @Override
     public void getRows(int sheetIndex, int curRow, List<String> rowList) {
         System.out.println(curRow + "  ");
@@ -12,4 +13,5 @@ public class ExcelRowReader implements IExcelRowReader {
             System.out.println(string.equals("") ? "*" : string + "  ");
         }
     }
+
 }
--- src/com/indrave/util/ExcelReadUtil.java
+++ src/com/indrave/util/ExcelReadUtil.java
@@ -8,9 +8,16 @@ public class ExcelReadUtil {
     //Excel2007扩展名
     public final static String EXCEL07_EXTENSION = ".XLSX";
 
+    /**
+     *读取Excel文件，可能为03版本也可能为07版本
+     * @param excelRowReader
+     * @param fileName
+     * @throws Exception
+     */
     public static void readExcel(IExcelRowReader excelRowReader, String fileName) throws Exception {
         if (fileName.endsWith(EXCEL03_EXTENSION)) {
             ExcelXlsReader excelXlsReader = new ExcelXlsReader();
+
         } else if (fileName.endsWith(EXCEL07_EXTENSION)) {
             ExcelXlxsReader excelXlxsReader = new ExcelXlxsReader();
         } else {
